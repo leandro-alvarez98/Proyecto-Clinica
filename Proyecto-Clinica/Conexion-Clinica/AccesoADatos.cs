b5aee2ff75c18d9412e0153af4206bf0c1c8c079
FanncuS4n <fancus4n@gmail.com>
2023-10-25 14:42:01 -0300

    AccesoDatos y Listar

---
+++ Proyecto-Clinica/Conexion-Clinica/AccesoADatos.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conexion_Clinica
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+        public AccesoDatos()
+        {
+            //Para nash xd
+            //conexion = new SqlConnection("server=.\\SQLEXPRESS; database=CLINICA; integrated security=true");
+            
+            //Lean y facu
+            conexion = new SqlConnection("server=.; database=CLINICA; integrated security=true");
+
+            comando = new SqlCommand();
+        }
+        public void setConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+        public void setProcedimiento(string sp)
+        {
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.CommandText = sp;
+        }
+
+
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        public void setParametro(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+        
+        public void cerrarConexion()
+        {
+            if (lector != null)
+                lector.Close();
+            conexion.Close();
+        }
+    }
+}
---
+++ Proyecto-Clinica/Conexion-Clinica/ClinicaConexion.cs
@@ -0,0 +1,48 @@
+﻿using Proyecto_Clinica.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conexion_Clinica
+{
+    internal class ClinicaConexion
+    {
+        public Clinica listar()
+        {
+            Clinica objetoClinica = new Clinica();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setConsulta("");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    objetoClinica.usuarios = new List<Usuario>();
+                    objetoClinica.pacientes = new List<Paciente>();
+                    objetoClinica.turnos = new List<Turno>();
+                    objetoClinica.medicos = new List<Medico>();
+
+                    // Primero hay que hacer los Listar() de todos los demás
+                    //Despues llenar las listas de Clinica con esas funciones
+
+                }
+                return objetoClinica;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+
+    }
+}
