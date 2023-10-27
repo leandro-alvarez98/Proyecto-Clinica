9897b16a4ba9d69103c2e64167e4e798357fc0b9
FanncuS4n <fancus4n@gmail.com>
2023-10-27 11:50:45 -0300

    Actualización Listar()
    Ahora se listan los Turnos, Usuarios y la Clínica se llena con información.
    A la query habrá que ajustarla conforme necesitemos más (o menos) datos.

--- Proyecto-Clinica/Dominio/Turno.cs
+++ Proyecto-Clinica/Dominio/Turno.cs
@@ -8,11 +8,11 @@ namespace Proyecto_Clinica.Dominio
     public class Turno
     {
         public int Id { get; set; }
-        public Paciente Paciente { get; set; }
-        public Medico Medico { get; set; }
+        public String Paciente { get; set; }
+        public String Medico { get; set; }
         public DateTime Fecha { get; set; }
         public DateTime HoraInicio { get; set; }
         public DateTime HoraFin { get; set; }
-        public string Estado { get; set; }
+        public bool Estado { get; set; }
     }
 }
