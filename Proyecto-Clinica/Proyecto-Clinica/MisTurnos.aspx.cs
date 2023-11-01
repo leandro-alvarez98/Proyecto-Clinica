934933644a63a3c13245272018a102e12be4ca80
FanncuS4n <fancus4n@gmail.com>
2023-11-01 20:17:06 -0300

    Pocos cambios
    ni da explicar (?

--- Proyecto-Clinica/Proyecto-Clinica/MisTurnos.aspx.cs
+++ Proyecto-Clinica/Proyecto-Clinica/MisTurnos.aspx.cs
@@ -14,22 +14,95 @@ namespace Proyecto_Clinica
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
-        Medico medicoActual; 
+        Medico medicoActual;
+        Paciente pacienteActual;
         List<Turno> misTurnos;
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
-                medicoActual = new Medico();
                 misTurnos = new List<Turno>();
 
-                Cargar_Médico();
-                Cargar_Turnos();
+                if(((Proyecto_Clinica.Dominio.Usuario)Session["Usuario"]).Tipo == "Médico")
+                {
+                    medicoActual = new Medico();
+
+                    Cargar_Médico();
+                    Cargar_TurnosMedicos();
+                }
+                else if(((Proyecto_Clinica.Dominio.Usuario)Session["Usuario"]).Tipo == "Paciente")
+                {
+                    pacienteActual = new Paciente();
+                    Cargar_Paciente();
+                    Cargar_TurnosPaciente();
+                }
 
                 dgv_Turnos.DataSource = misTurnos;
                 dgv_Turnos.DataBind();
             }
         }
+
+
+        private void Cargar_Paciente()
+        {
+            int IdUsuario = ((Usuario)Session["Usuario"]).Id;
+
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setConsulta("SELECT ID_PACIENTE, ID_USUARIO, NOMBRE, APELLIDO, TELEFONO, DIRECCION, FECHA_NACIMIENTO, MAIL, ESTADO FROM PACIENTES P");
+                datos.setParametro("@IDUSUARIO", IdUsuario);
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    //pacienteActual. 
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+        private void Cargar_TurnosPaciente()
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setConsulta("SELECT T.ID_TURNO AS ID, M.NOMBRE + ', ' + m.APELLIDO AS MAPENOM, p.NOMBRE + ', ' + p.APELLIDO AS PAPENOM, t.FECHA AS FECHA, t.HORA_INICIO AS HORAINICIO, t.HORA_FIN AS HORAFIN, t.ESTADO AS ESTADO FROM PACIENTES P INNER JOIN TURNOS T ON T.ID_PACIENTE = P.ID_PACIENTE INNER JOIN MEDICOS M ON M.ID_MEDICO = T.ID_MEDICO WHERE P.ID_PACIENTE = @IDPACIENTE");
+                datos.setParametro("@IDPACIENTE", pacienteActual.Id);
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Turno turno = new Turno();
+                    turno.Id = (int)datos.Lector["ID"];
+                    turno.Medico = (String)datos.Lector["MAPENOM"];
+                    turno.Paciente = (String)datos.Lector["PAPENOM"];
+                    turno.Fecha = (DateTime)datos.Lector["FECHA"];
+                    turno.HoraInicio = (TimeSpan)datos.Lector["HORAINICIO"];
+                    turno.HoraFin = (TimeSpan)datos.Lector["HORAFIN"];
+                    turno.Estado = (String)datos.Lector["ESTADO"];
+
+                    misTurnos.Add(turno);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
         private void Cargar_Médico()
         {
             int IdUsuario = ((Usuario)Session["Usuario"]).Id;
@@ -70,7 +143,7 @@ namespace Proyecto_Clinica
                 datos.cerrarConexion();
             }
         }
-        private void Cargar_Turnos()
+        private void Cargar_TurnosMedicos()
         {
             AccesoDatos datos = new AccesoDatos();
             try
@@ -102,7 +175,6 @@ namespace Proyecto_Clinica
             {
                 datos.cerrarConexion();
             }
-
         }
     }
 }
