6957607bc0c23bb3ae6ae0cb4c7988e9967ee65f
FanncuS4n <fancus4n@gmail.com>
2023-11-22 12:35:56 -0300

    Cambiar Imagen en PerfilUsuario
    Sólo el front, queda hacer la configuracion en el backend

--- Proyecto-Clinica/Proyecto-Clinica/Perfil_Usuario.aspx.designer.cs
+++ Proyecto-Clinica/Proyecto-Clinica/Perfil_Usuario.aspx.designer.cs
@@ -175,5 +175,23 @@ namespace Proyecto_Clinica
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnCambioContraseña;
+
+        /// <summary>
+        /// Control txtImagen.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputFile txtImagen;
+
+        /// <summary>
+        /// Control imgPerfil.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image imgPerfil;
     }
 }
