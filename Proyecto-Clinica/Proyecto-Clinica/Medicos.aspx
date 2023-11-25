0b0e617ba33a2c347d72dfd51438ffedc19b7093
FanncuS4n <fancus4n@gmail.com>
2023-11-25 12:34:48 -0300

    Página con médicos
    Una nueva pagina disponible sólo para Admins y Recepcionistas que lista médicos y permitirá manejar sus horarios y especialidades

---
+++ Proyecto-Clinica/Proyecto-Clinica/Medicos.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master-page.Master" AutoEventWireup="true" CodeBehind="Medicos.aspx.cs" Inherits="Proyecto_Clinica.Medicos" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <asp:Repeater ID="repeaterMedicos" runat="server">
+        <ItemTemplate>
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><%# Eval("Nombre") %>, <%# Eval("Apellido") %></h5>
+                    <p class="card-text">Jornada/s</p>
+                    <p class="card-text">Especialidad/es</p>
+                    <a href="#" class="btn btn-primary">Editar</a>
+                </div>
+            </div>
+        </ItemTemplate>
+    </asp:Repeater>
+
+
+</asp:Content>
---
+++ Proyecto-Clinica/Proyecto-Clinica/Usuario.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_Clinica.Dominio
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Contraseña { get; set; }
+        public string Tipo { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Dni {  get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public DateTime Fecha_Nacimiento { get; set; }
+        public string Mail { get; set; }
+        public bool Estado { get; set; }
+        public int Id_Imagen { get; set; }
+        public string Imagen { get; set; }
+        public Usuario()
+        {
+            Id = -1;
+            Dni=00000000;
+            Nombre = "No especificado";
+            Contraseña = "No especificado";
+            Tipo = "No especificado";
+            Nombre = "No especificado";
+            Apellido = "No especificado";
+            Telefono = "No especificado";
+            Direccion = "No especificado";
+            Fecha_Nacimiento = new DateTime();
+            Mail = "No especificado";
+            Estado = true;
+            Imagen = "NoImagen";
+        }
+        
+    }
+}
